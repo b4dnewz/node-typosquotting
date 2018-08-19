f577198cb67b47b66531e152eb7ea24bdf0e858f
Filippo Conti <filippo@codekraft.it>
2018-08-19 17:41:28 +0200

    Updated project config
    Changed ignore folders config and added webpack for browser bundle

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  mode: 'production',
+  entry: path.resolve(__dirname, 'lib/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    library: 'typosquotter'
+  }
+};
